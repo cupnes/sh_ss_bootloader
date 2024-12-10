236d4f16d7daa6fade475130a046fa03434774c6
Yuma Ohgami <yuma@ohgami.jp>
2024-12-10 10:58:03 +0900

    test/clear_startup_nbg0.sh: delete 4bytes each

--- test/clear_startup_nbg0.sh
+++ test/clear_startup_nbg0.sh
@@ -5,14 +5,14 @@
 set -ue
 
 while read delete_area_first delete_area_last; do
-	./clear_range.sh word "25e0$delete_area_first" "25e0$delete_area_last"
+	./clear_range.sh long "25e0$delete_area_first" "25e0$delete_area_last"
 done <<EOF
-6498 64ba
-6518 653a
-6598 65ba
-6618 663a
-6698 66ba
-6718 673a
+6498 64b8
+6518 6538
+6598 65b8
+6618 6638
+6698 66b8
+6718 6738
 6988 69c8
 6a08 6a48
 6b14 6b3c
