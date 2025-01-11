e2f39e61fbda89ddadfa2079f816be88d59d8f97
Yuma Ohgami <yuma@ohgami.jp>
2025-01-11 18:35:59 +0900

    test: add dump_1st_tile_data.sh.

---
+++ test/dump_1st_tile_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ue
+
+./peek.sh long 25e01400
+./peek.sh long 25e01404
+./peek.sh long 25e01408
+./peek.sh long 25e0140c
+./peek.sh long 25e01410
+./peek.sh long 25e01414
+./peek.sh long 25e01418
+./peek.sh long 25e0141c
